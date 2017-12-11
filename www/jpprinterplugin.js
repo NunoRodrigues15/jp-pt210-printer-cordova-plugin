0b1247819c6f50dee4e58ddaec81b8724edf068b
Nuno Rodrigues <nuno.rodrigues@factory.odkas.com>
2017-12-11 11:09:20 +0000

    updates By André

---
+++ www/jpprinterplugin.js
@@ -0,0 +1,26 @@
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = 'JpPrinterPlugin';
+
+var JpPrinterPlugin = {
+  echo: function(phrase, cb) {
+    exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
+  },
+  initBroadcast: function(cb) {
+    exec(cb, null, PLUGIN_NAME, 'initBroadcast', []);
+  },
+  connect: function(phrase, cb) {
+    exec(cb, null, PLUGIN_NAME, 'connect', []);
+  },
+  connectTo: function(phrase, cb) {
+    exec(cb, null, PLUGIN_NAME, 'connectTo', [phrase]);
+  },
+  printText: function(phrase, cb) {
+    exec(cb, null, PLUGIN_NAME, 'printText', [phrase]);
+  },
+  disconnect: function(phrase, cb) {
+    exec(cb, null, PLUGIN_NAME, 'disconnect', []);
+  }
+}
+
+module.exports = JpPrinterPlugin;
